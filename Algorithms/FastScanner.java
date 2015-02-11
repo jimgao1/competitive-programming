c47063a0719869d74fdd1bcecc8d885737761c67
jimthenerd <jimthenerd@hotmail.com>
2015-02-11 16:32:05 -0500

    Create FastScanner.java

---
+++ Algorithms/FastScanner.java
@@ -0,0 +1,25 @@
+public static class FastScanner {
+        BufferedReader br;
+        StringTokenizer st;
+
+        FastScanner() {
+            try {
+                br = new BufferedReader(new InputStreamReader(System.in));
+            } catch (Exception ex){}
+        }
+
+        String next() {
+            while (st == null || !st.hasMoreTokens()) {
+                try {
+                    st = new StringTokenizer(br.readLine());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            return st.nextToken();
+        }
+
+        int nextInt() {
+            return Integer.parseInt(next());
+        }
+	}
