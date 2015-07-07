7e8d0d924af879010623371a1e13798aace486ce
Jim Gao <jimthenerd@hotmail.com>
2015-07-07 15:04:16 -0400

    Create fractions.cpp

---
+++ Algorithms/fractions.cpp
@@ -0,0 +1,40 @@
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int gcd(int a, int b) {
+    return b == 0 ? a : gcd(b, a % b);
+}
+
+int lcm(int a, int b) {
+    return gcd(a, b) ? (a / gcd(a, b) * b) : 0;
+}
+
+struct fraction{
+    int numerator, denominator;
+
+    fraction(int a, int b){
+        numerator = a, denominator = b;
+    }
+
+    fraction simplify(fraction f){
+        int g = gcd(f.numerator, f.denominator);
+        return fraction(f.numerator / g, f.denominator / g);
+    }
+
+    fraction add(fraction f){
+        int l = lcm(denominator, f.denominator);
+        numerator *= (l / denominator);
+        numerator += f.numerator * (l / f.denominator);
+
+        return simplify(fraction(numerator, l));
+    }
+
+    void print(){
+        if (denominator != 1)
+            printf("%d %d\n", numerator, denominator);
+        else
+            printf("%d\n", numerator);
+    }
+};
