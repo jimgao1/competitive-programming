8c651bf39abe8499a63f94eb610581a7ebe3c834
Jim Gao <jimthenerd@hotmail.com>
2015-08-04 19:38:59 -0400

    Create coci061p2.cpp

---
+++ WCIPEG_Problem/coci061p2.cpp
@@ -0,0 +1,17 @@
+
+#include <bits/stdc++.h>
+
+#define PI 3.1415926535
+
+using namespace std;
+
+double R;
+
+int main(){
+    cin >> R;
+
+    cout << fixed << setprecision(6) << PI * R * R << endl;
+    cout << fixed << setprecision(6) << 2 * R * R << endl;
+
+    return 0;
+}
