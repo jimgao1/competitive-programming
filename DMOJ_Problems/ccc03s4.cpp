590917188befcd00e36debe3e6279bb758a31faa
Jim Gao <jimthenerd@hotmail.com>
2015-12-26 19:51:00 -0500

    Create ccc03s4.cpp

---
+++ DMOJ_Problems/ccc03s4.cpp
@@ -0,0 +1,45 @@
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+bool cmp(string a, string b){
+    for (int i = 0; i < min(a.length(), b.length()); i++)
+        if (a[i] != b[i])
+            return a[i] < b[i];
+    return a.length() < b.length();
+}
+
+int main(){
+    int T;
+    cin >> T;
+
+    while(T--){
+        string str;
+        cin >> str;
+
+        vector<string> sub;
+
+        for (int i = 0; i < str.length(); i++){
+            sub.push_back(str.substr(i, str.length() - i));
+        }
+
+        sort(sub.begin(), sub.end(), cmp);
+        int cnt = sub[0].length();
+
+        for (int i = 0; i < sub.size() - 1; i++){
+            int scnt = 0;
+
+            for (int j = 0; j < min(sub[i].length(), sub[i + 1].length()); j++){
+                if (sub[i][j] == sub[i + 1][j])
+                    scnt++;
+                else
+                    break;
+            }
+
+            cnt += (sub[i + 1].length() - scnt);
+        }
+
+        printf("%d\n", cnt + 1);
+    }
+}
