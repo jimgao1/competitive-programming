62f2d5b643b187a69ab0bec9d5cffd4c09e9f4af
Jim Gao <jim@jimgao.tk>
2016-10-22 18:52:16 -0400

    Created dmopc16c1p3.cpp

---
+++ DMOPC/dmopc16c1p3.cpp
@@ -0,0 +1,33 @@
+
+#include <bits/stdc++.h>
+
+#define INF 0x3f3f3f3f
+#define MAXN 10005
+
+using namespace std;
+
+int N;
+double dp[MAXN], val[MAXN];
+
+int main(){
+    //freopen("input.txt", "r", stdin);
+
+    for (int i = 0; i < MAXN; i++) dp[i] = INF;
+
+    cin >> N;
+    for (int i = 1; i <= N; i++) cin >> val[i];
+
+    val[0] = dp[0] = 0;
+    for (int i = 1; i <= N + 1; i++){
+        dp[i] = val[i] + dp[i - 1];
+        if (i - 2 >= 0)
+            dp[i] = min(dp[i], dp[i - 2] + (val[i] + val[i - 1] - min(val[i], val[i - 1]) / 2.0));
+        if (i - 3 >= 0)
+            dp[i] = min(dp[i], dp[i - 3] + (val[i] + val[i - 1] + val[i - 2] - min(val[i], min(val[i - 1], val[i - 2]))));
+
+        //printf("i = %d, val[i] = %.1f, dp[i] = %.1f\n", i, val[i], dp[i]);
+    }
+
+    printf("%.1f\n", dp[N + 1]);
+}
+
