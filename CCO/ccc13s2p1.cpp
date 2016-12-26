1813698f0326fa48188463df74c22a303dc2b67f
Jim Gao <jim@jimgao.tk>
2016-12-25 22:18:36 -0500

    oops

--- CCO/ccc13s2p1.cpp
+++ CCO/ccc13s2p1.cpp
@@ -1,82 +1,38 @@
-/*
-	This code does NOT work for all the cases. Will update later.
-*/
-
-#include <iostream>
-#include <string>
-#include <algorithm>
-#include <vector>
-#include <cmath>
-#include <cstdlib>
+[200~
+#include <bits/stdc++.h>
 
 using namespace std;
 
-int x;
+int N;
+vector<int> ans;
 
-bool palindrom(vector<int> num){
-	for (int i = 0; i < ceil(num.size() / 2.0); i++){
-		if (num[i] != num[num.size() - i - 1])
-			return false;
-	}
-	return true;
-}
-
-vector<int> base_convert(vector<int> orig, int src, int tar){
-	long sum = 0;
-	for (int i = 0; i < orig.size(); i++)
-		sum += orig[i] * pow(src, i);
-
-	vector<int> result;
-
-	while (sum > 0){
-		int res = sum % tar;
-		sum = sum / tar;
-		result.push_back(res);
-	}
-
-	reverse(result.begin(), result.end());
-
-	return result;
-}
-
-vector<int> defaultToBase(long n){
-	vector<int> number;
-
-	while (n > 0){
-		number.push_back(n % 10);
-		n = n / 10;
-	}
-
-	//reverse(number.begin(), number.end());
-
-	return number;
-}
-
-void print_number(vector<int> number){
-	for (int n : number)
-		cout << n << " ";
-	cout << endl;
+bool palindrome(int num, int base){
+    vector<int> digits;
+    while(num > 0){
+        digits.push_back(num % base);
+        num /= base;
+    }
+    for (int i = 0; i < digits.size() / 2; i++){
+        if (digits[i] != digits[digits.size() - i - 1])
+            return false;
+    }
+    return true;
 }
 
 int main(){
-	
-	long num;
-	cin >> num;
+    cin >> N;
+    for (int i = 2; i <= sqrt(N); i++){
+        if (palindrome(N, i)) ans.push_back(i);
+    }
 
-	for (int i = 2; i < num+1; i++){
-		if (i > ceil(sqrt(num))){
-			double v = num / (double)(i);
-			if (v == (int)(v)){
-				printf("%d\n", i);
-			}
-		}
-		else {
-			if (palindrom(base_convert(defaultToBase(num), 10, i))){
-				printf("%d\n", i);
-			}
-		}
-	}
+    for (int i = 1; i <= sqrt(N); i++){
+        if ((N - i) % i == 0 && N % ((N - i) / i) != 0){
+            ans.push_back((N - i) / i);
+        }
+    }
 
+    sort(ans.begin(), ans.end());
+    ans.erase(unique(ans.begin(), ans.end()), ans.end());
 
-	return 0;
-}
+    for (int i : ans) cout << i << endl;
+}[201~
