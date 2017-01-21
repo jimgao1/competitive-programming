6e89ea0a54fc447661b53dc0b1a5c9bc13363337
Jim Gao <jim@jimgao.tk>
2017-01-21 17:27:07 -0500

    Created ccc16s5.cpp

--- CCC/ccc15s5.cpp
+++ CCC/ccc15s5.cpp
@@ -1,73 +1,30 @@
-
 #include <bits/stdc++.h>
 
-#define MAXN 3200
 #define ll long long
 
 using namespace std;
 
-int N, M, pa[MAXN], pb[120];
-int dp[MAXN][120][120][2];
+ll N, T;
+string state;
 
-int f(int cur, int l, int r, int taken){
-    //printf("cur = %d, l = %d, r = %d, taken = %d\n", cur, l, r, taken);
-
-    if (dp[cur][l][r][taken] != -1)
-        return dp[cur][l][r][taken];
-
-    int ans = 0;
-
-    //Case 5: at the end of the line
-    if (cur == N){
-        //Check if the second pile is empty
-        if (l > r){
-            return ans = 0;
-        } else {
-            if (taken){
-                return dp[cur][l][r][taken] = f(cur, l + 1, r, 0);
-            } else {
-                return dp[cur][l][r][taken] = pb[r] + f(cur, l, r - 1, 1);
-            }
-        }
+void calc(int k){
+    stringstream ss;
+    for (int i = 0; i < N; i++){
+        bool a = state[((i - (1LL << k)) % N + N) % N] == '1';
+        bool b = state[(i + (1LL << k)) % N] == '1';
+        ss << ((a ^ b) ? "1" : "0");
     }
-
-    if (taken){
-        //Case 2: skip the current pie
-        ans = f(cur + 1, l, r, 0);
-
-        if (l <= r){
-            //Case 3: insert separator
-            ans = max(ans, f(cur, l + 1, r, 0));
-        }
-    } else {
-        //Case 1: take the current pie
-        ans = pa[cur] + f(cur + 1, l, r, 1);
-
-        //Case 2: skip the current pie
-        ans = max(ans, f(cur, l, r, 1));
-
-        if (l <= r){
-            //Case 4: eat pie from pb
-            ans = max(ans, pb[r] + f(cur, l, r - 1, 1));
-        }
-    }
-
-
-    return dp[cur][l][r][taken] = ans;
+    state = ss.str();
 }
 
 int main(){
-    cin >> N;
-    for (int i = 0; i < N; i++){
-        cin >> pa[i];
-    }
-    cin >> M;
-    for (int i = 1; i <= M; i++){
-        cin >> pb[i];
-    }
-    sort(pb + 1, pb + M + 1);
-    memset(dp, -1, sizeof dp);
+    cin >> N >> T >> state;
 
-    cout << f(0, 1, M, 0) << endl;
+    for (int i = 0; i < 61; i++){
+        if (T & (1LL << i)){
+            calc(i);
+        }
+    }
+
+    cout << state << endl;
 }
-
